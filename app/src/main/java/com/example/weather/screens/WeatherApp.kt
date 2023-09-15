baa1ad57f31933b217170de1f131bd4de6db6102
Joshua <joshuagutierrez724@gmail.com>
2023-09-15 10:34:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/weather/screens/WeatherApp.kt
@@ -0,0 +1,18 @@
+package com.example.weather.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun weatherApp(){
+
+}
+
+@Composable
+fun currentWeatherCard(){
+
+}
+
+@Composable
+fun hourlyForecast(){
+
+}
---
+++ app/src/main/java/com/example/weather/screens/WeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weather.screens
+
+import androidx.lifecycle.ViewModel
+
+class WeatherViewModel : ViewModel() {
+
+}
